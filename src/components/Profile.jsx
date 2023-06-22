7be7c8ca93b2095f21a2dc22e52eb9edefe7eda7
AnandaDwiLaksono <anandad182@gmail.com>
2023-06-22 23:00:35 +0700

    consume user, category, and payment api

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,163 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
-import avatar from '../assets/img/Logo_Unicloud.jpg';
+import { API_URL } from '../config/apiConfig';
 
 const Profile = () => {
+	const [user, setUser] = useState({});
+	const [categories, setCategories] = useState([]);
+	const [payments, setPayments] = useState([]);
+	const [logo, setLogo] = useState(null);
+	const [newCategory, setNewCategory] = useState([]);
+	const [oldCategory, setOldCategory] = useState([]);
+	const [newPayment, setNewPayment] = useState([]);
+	const [oldPayment, setOldPayment] = useState([]);
+
+	useQuery({
+		queryKey: ["user"],
+		queryFn: async () => {
+			return await axios.get(`${API_URL}/api/users`);
+		},
+		onSuccess: (data) => {
+			setUser(data.data.data[0]);
+		},
+		onError: (error) => {
+			console.log(error);
+		},
+	});
+
+	useQuery({
+		queryKey: ["category"],
+		queryFn: async () => {
+			return await axios.get(`${API_URL}/api/categories`);
+		},
+		onSuccess: (data) => {
+			setCategories(data.data.data);
+		},
+		onError: (error) => {
+			console.log(error);
+		},
+	});
+
+	useQuery({
+		queryKey: ["payment"],
+		queryFn: async () => {
+			return await axios.get(`${API_URL}/api/payment_types`);
+		},
+		onSuccess: (data) => {
+			setPayments(data.data.data);
+		},
+		onError: (error) => {
+			console.log(error);
+		},
+	});
+
+	const editProfile = useMutation({
+		mutationFn: async (data) => {
+			return await axios.put(`${API_URL}/api/users/1`, data);
+		},
+	});
+
+	const editLogo = useMutation({
+		mutationFn: async (data) => {
+			return await axios.put(`${API_URL}/api/users/logo/1`, {"logo": data});
+		},
+	});
+
+	const addCategory = useMutation({
+		mutationFn: async (data) => {
+			return await axios.post(`${API_URL}/api/categories`, data);
+		},
+	});
+
+	const deleteCategory = useMutation({
+		mutationFn: async (data) => {
+			return await axios.delete(`${API_URL}/api/categories/${data}`);
+		},
+	});
+
+	const addPayment = useMutation({
+		mutationFn: async (data) => {
+			return await axios.post(`${API_URL}/api/payment_types`, data);
+		},
+	});
+
+	const deletePayment = useMutation({
+		mutationFn: async (data) => {
+			return await axios.delete(`${API_URL}/api/payment_types/${data}`);
+		},
+	});
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+		const reader = new FileReader();
+
+		reader.onloadend = () => {
+			setLogo(reader.result);
+		};
+
+		reader.readAsDataURL(file);
+  };
+
+	const handleChangeCategory = (event) => {
+		const dataString = event.target.value;
+		const dataArray = dataString.split(", ");
+
+		const addCategory = dataArray.filter(item => !categories.some(category => category.name === item));
+		const deleteCategory = categories.filter(item => !dataArray.some(name => name === item.name)).map(item => item.id);
+
+		setNewCategory(addCategory);
+		setOldCategory(deleteCategory);
+	};
+
+	const handleChangePayment = (event) => {
+		const dataString = event.target.value;
+		const dataArray = dataString.split(", ");
+
+		const addPayment = dataArray.filter(item => !payments.some(payment => payment.name === item));
+		const deletePayment = payments.filter(item => !dataArray.some(name => name === item.name)).map(item => item.id);
+
+		setNewPayment(addPayment);
+		setOldPayment(deletePayment);
+	};
+
+	const onClickSaveHandler = () => {
+		editProfile.mutate(user);
+
+		if (logo) {
+			editLogo.mutate(logo, {
+				onSuccess: () => {
+					window.location.reload();
+				},
+			});
+		}
+
+		if (newCategory.length > 0) {
+			newCategory.forEach(item => {
+				addCategory.mutate({"name": item});
+			});
+		}
+
+		if (oldCategory.length > 0) {
+			oldCategory.forEach(item => {
+				deleteCategory.mutate(item);
+			});
+		}
+
+		if (newPayment.length > 0) {
+			newPayment.forEach(item => {
+				addPayment.mutate({"name": item});
+			});
+		}
+
+		if (oldPayment.length > 0) {
+			oldPayment.forEach(item => {
+				deletePayment.mutate(item);
+			});
+		}
+	};
+
 	return (
 		<div className='p-6 relative'>
 			<div className='flex items-center min-h-[18.75rem] bg-center bg-cover bg-profile rounded-2xl overflow-hidden'></div>
@@ -11,7 +166,7 @@ const Profile = () => {
 					<div className='justify-center leading-[1] select-none transition-all duration-200 ease-in-out delay-[0ms] rounded-xl font-bold shadow w-[4.625rem] h-[4.625rem] text-base'>
 						<img 
 							className='w-full text-center object-cover indent-[10000px] h-auto rounded-xl'
-							src={avatar}
+							src={user.logo}
 							alt='photo_profile'
 						/>
 					</div>
@@ -46,7 +201,7 @@ const Profile = () => {
 								<div className='flex flex-row gap-8 align-middle items-center'>
 									<img 
 										className='text-center object-cover indent-[10000px] rounded-xl shadow w-[4.625rem] h-[4.625rem]'
-										src={avatar}
+										src={user.logo}
 										alt='photo_profile'
 									/>
 									<input
@@ -54,6 +209,7 @@ const Profile = () => {
 										id="img"
 										name="img"
 										accept="image/*"
+										onChange={handleFileChange}
 									/>
 								</div>
 							</div>
@@ -68,7 +224,10 @@ const Profile = () => {
 									type='text'
 									name='name'
 									className='border-solid border-1 w-[50%]'
-									defaultValue='Unicloud'
+									defaultValue={user.name}
+									onChange={(e) => {
+										setUser({ ...user, name: e.target.value });
+									}}
 								/>
 							</div>
 							<label
@@ -82,7 +241,10 @@ const Profile = () => {
 										type='text'
 										name='no'
 										className='border-solid border-1 w-[50%]'
-										defaultValue='081818683844'
+										defaultValue={user.phone_number}
+										onChange={(e) => {
+											setUser({ ...user, phone_number: e.target.value });
+										}}
 									/>
 							</div>
 							<label
@@ -96,7 +258,10 @@ const Profile = () => {
 									type='text'
 									name='email'
 									className='border-solid border-1 w-[50%]'
-									defaultValue='unicloud@gmail.com'
+									defaultValue={user.email}
+									onChange={(e) => {
+										setUser({ ...user, email: e.target.value });
+									}}
 								/>
 							</div>
 							{/* <label
@@ -162,7 +327,10 @@ const Profile = () => {
 							<div className='pt-1 px-4 pb-4'>
 								<textarea
 									className='border-solid border-1 w-[50%]'
-									defaultValue='Jl.pattimura gang 1 No.1, RT.003/RW.008, Sisir, Kec. Batu, Kota Batu, Jawa Timur 65315'
+									defaultValue={user.address}
+									onChange={(e) => {
+										setUser({ ...user, address: e.target.value });
+									}}
 								/>
 							</div>
 							<div className='pt-4 px-4 text-[#344767]'>
@@ -181,7 +349,8 @@ const Profile = () => {
 									type='text'
 									name='email'
 									className='border-solid border-1 w-[50%]'
-									defaultValue='Mod, Pod, Coil, Aksesoris, Freebase, Salt Nic'
+									defaultValue={categories.map(category => category.name).join(', ')}
+									onChange={handleChangeCategory}
 								/>
 							</div>
 							<label
@@ -195,17 +364,19 @@ const Profile = () => {
 									type='text'
 									name='email'
 									className='border-solid border-1 w-[50%]'
-									defaultValue='Cash, QRIS, BCA'
+									defaultValue={payments.map(payment => payment.name).join(', ')}
+									onChange={handleChangePayment}
 								/>
 							</div>
-							{/* <div className='p-4 text-right'>
+							<div className='p-4 text-right'>
 								<button
 									type='button'
 									className='bg-green-300 rounded-lg px-4 py-1 text-green-700 text-base tracking-wider font-semibold shadow-md hover:scale-105 hover:bg-green-200 hover:text-green-600'
+									onClick={onClickSaveHandler}
 								>
 									Save
 								</button>
-							</div> */}
+							</div>
 						</div>
 					</div>
 				</div>
