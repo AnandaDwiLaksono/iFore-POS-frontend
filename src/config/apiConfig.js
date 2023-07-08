e294979ab946227f2959b6cab660a061c71cba43
AnandaDwiLaksono <anandad182@gmail.com>
2023-07-09 02:29:29 +0700

    change api url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,4 +1,2 @@
-export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://ifore-api.up.railway.app'
-    : 'http://localhost:3002';
+// export const API_URL = 'http://localhost:3002';
+export const API_URL = 'https://ifore-api.up.railway.app';
